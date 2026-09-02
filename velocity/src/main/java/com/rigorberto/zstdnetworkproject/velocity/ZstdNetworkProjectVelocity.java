b03e317cd0007b0a4a562bd57d90b55931cabaa9
Rigorberto <na@na>
2026-09-01 22:49:19 -0600

    beta-2.2 release: version bump (beta-2.1 -> beta-2.2)

--- velocity/src/main/java/com/rigorberto/zstdnetworkproject/velocity/ZstdNetworkProjectVelocity.java
+++ velocity/src/main/java/com/rigorberto/zstdnetworkproject/velocity/ZstdNetworkProjectVelocity.java
@@ -46,7 +46,7 @@ import java.util.concurrent.TimeUnit;
 @Plugin(
     id = "zstdnetworkproject",
     name = "ZstdNetworkProject",
-    version = "beta-2.1",
+    version = "beta-2.2",
     description = "Zstd packet compressor for Velocity proxy (compatible with Krypton & PacketFixer)",
     authors = {"Rigorberto"}
 )
